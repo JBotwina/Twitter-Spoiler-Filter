1f3d049763fe680f488b6e427d1597a4e10746e7
mandlerm <amaya.melissa@gmail.com>
2019-04-25 07:59:22 -0500

    created homePageHandler class

---
+++ src/main/java/HomePageHandler.java
@@ -0,0 +1,10 @@
+//can we change teh name of the default package to something else?
+// how did we end up with default package vs. a named package?
+
+import spark.request;
+import spark.route;
+import spark.response;
+
+public class HomePageHandler implements Route {
+	return "welcome";
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,12 +12,15 @@ public class Main {
     public static void main(String[] args) {
     	port(8080);
     	
+    	HomePageHandler homePageHandler = new HomePageHandler();
     	/**
     	 * Input: String
     	 * Output: string
     	 * Practice get request to the local page, first argument is the page to make the request to
     	 * Second argument is what is being placed on the page.
     	 */
-        get("/hello", (req, res) -> "Hello World said the dog to Carla");
+        
+    	get("/", homePageHandler) ;
+    	//get("/hello", (req, res) -> "Hello World said the dog to Carla");
     }
 }

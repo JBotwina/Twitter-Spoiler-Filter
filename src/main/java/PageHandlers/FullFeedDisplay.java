17e94cb410b4138be8de42e2fbe50721ea1c8f89
mandlerm <amaya.melissa@gmail.com>
2019-04-27 12:18:00 -0500

    added header for fullfeed page

--- src/main/java/PageHandlers/FullFeedDisplay.java
+++ src/main/java/PageHandlers/FullFeedDisplay.java
@@ -5,11 +5,11 @@ import spark.Response;
 import spark.Route;
 
 public class FullFeedDisplay implements Route{
-
+	private final String header = "<h1>Full Twitter Feed</h1>";
 	private final String homeButton = "<button type=\"button\" onclick=\"location.href='/'\">Home</button>";
 	public Object handle(Request request, Response response) throws Exception {
 		
-		return homeButton;
+		return header + homeButton;
 	}
 
 }

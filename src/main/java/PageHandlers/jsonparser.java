bb885d77634381e1a7a426d6d3b711cc40286b73
mandlerm <amaya.melissa@gmail.com>
2019-04-28 17:53:31 -0500

    changed json choice

--- src/main/java/PageHandlers/jsonparser.java
+++ src/main/java/PageHandlers/jsonparser.java
@@ -2,12 +2,16 @@ package PageHandlers;
 
 import java.util.ArrayList;
 
-import org.json.simple.JSONArray; 
-import org.json.simple.JSONObject; 
-import org.json.simple.parser.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONString;
+
+//import org.json.simple.parser.*;
 
 import twitter4j.Status; 
 
+@SuppressWarnings("unused")
 public class jsonparser {
 	
 	public static String parseJson(ArrayList<Status> statuses) {

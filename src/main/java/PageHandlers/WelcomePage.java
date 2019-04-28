db5ce544da91162cc71ad26b58576643d8408e9f
mandlerm <amaya.melissa@gmail.com>
2019-04-28 04:15:39 -0500

    added action and method to homepage form

--- src/main/java/PageHandlers/WelcomePage.java
+++ src/main/java/PageHandlers/WelcomePage.java
@@ -17,12 +17,13 @@ public class WelcomePage implements Route {
 			"<div text-align: center><h1>Welcome to Twitter Filter by</h1>\n" + 
 			"<h2>James Botwina, Jhon Valencia, Melissa Amaya</h2>\n" + 
 			"<p>For our MCIT 591 Final Class Project, Spring 2019</p>";
-	private String keyWordForm = "<form><input type=\"text\" action=\"\" name=\"keyword\"placeholder=\"enter keyword(s) to filter\" ><br><input type=\"submit\" value=\"Show me my filtered feed\"></form>";
+	private final String instructions = "<p>You can choose to view your full, unfiltered, Twitter feed, or enter keywords that you want filtered out of your feed for a period of time. </p>";
+	private String keyWordForm = "<form action=\"/filteredFeed\" method=\"post\"><input type=\"text\" name=\"keyword\"placeholder=\"enter keyword(s) to filter\" ><br><input type=\"submit\" value=\"Show me my filtered feed\"></form>";
 	
 	private String buttonFullFeed = "<button >Show full feed</button></div></html>";
 	
 	public Object handle(Request request, Response response) throws Exception {
-		return header + keyWordForm + buttonFullFeed;
+		return header + instructions + keyWordForm + buttonFullFeed;
 	}
 	
 }

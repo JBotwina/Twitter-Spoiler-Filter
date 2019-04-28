bc73087b7df3d94e6fba5fb76829dc5c507efd48
mandlerm <amaya.melissa@gmail.com>
2019-04-28 04:26:07 -0500

    changed backendhandler methods to return arraylist instead of store as instance variable

--- src/main/java/PageHandlers/BackEndHandler.java
+++ src/main/java/PageHandlers/BackEndHandler.java
@@ -7,24 +7,21 @@ import twitter4j.Status;
 
 public class BackEndHandler {
 	private TwitterExample te;
-	private static ArrayList<Status> badStatuses;
-	private static ArrayList<Status> fullFeed;
-	private static ArrayList<Status> filteredFeed;
-	
+
 	
 	public BackEndHandler() {
 		this.te = new TwitterExample();
 	}
 
-	public static void getBadStatuses() {
-		badStatuses = te.getBadStatuses();	
+	public static ArrayList<Status> getBadStatuses() {
+		return te.getBadStatuses();	
 	}
 	
-	public static void fullFeed() {
-		fullFeed = te.getUnfilteredStatuses();
+	public static ArrayList<Status> fullFeed() {
+		return te.getUnfilteredStatuses();
 	}
 	
-	public static void filteredFeed() {
-		filteredFeed = te.getGoodStatuses();
+	public static ArrayList<Status> filteredFeed() {
+		return te.getGoodStatuses();
 	}
 }
--- src/main/java/PageHandlers/FullFeedDisplay.java
+++ src/main/java/PageHandlers/FullFeedDisplay.java
@@ -1,12 +1,22 @@
 package PageHandlers;
 
+import java.util.ArrayList;
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import twitter4j.Status;
 
 public class FullFeedDisplay implements Route{
 	private final String header = "<h1>Full Twitter Feed</h1>";
 	private final String homeButton = "<button type=\"button\" onclick=\"location.href='/'\">Home</button>";
+	private ArrayList<Status> tweets;
+	
+	public FullFeedDisplay() {
+		tweets = BackEndHandler.fullFeed();
+	}
+	
+	
 	public Object handle(Request request, Response response) throws Exception {
 		
 		return header + homeButton;

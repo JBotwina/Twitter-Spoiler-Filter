9100f90f4600ce06c7ec8da54b9736536ac48acc
mandlerm <amaya.melissa@gmail.com>
2019-04-30 20:47:40 -0500

    header and home button now displaying on full tweet feed

--- src/main/java/PageHandlers/FullFeedDisplay.java
+++ src/main/java/PageHandlers/FullFeedDisplay.java
@@ -10,19 +10,17 @@ import twitter4j.Status;
 public class FullFeedDisplay implements Route{
 	private final String header = "<h1>Full Twitter Feed</h1>";
 	private final String homeButton = "<button type=\"button\" onclick=\"location.href='/'\">Home</button>";
-	private ArrayList<Status> tweets;
+	private String tweets;
 	
-	public HiddenFeed(BackEndHandler backEndHandlerParam) {
-		tweets = backEndHandlerParam.tweetToString(backEndHandlerParam.getBadStatuses());
+
+	public FullFeedDisplay() {
+		tweets = BackEndHandler.fullFeed();
 	}
-//	public FullFeedDisplay() {
-//		tweets = BackEndHandler.fullFeed();
-//	}
 //	
 	
 	public Object handle(Request request, Response response) throws Exception {
 		
-		return header + homeButton;
+		return header + homeButton + tweets;
 	}
 
 }

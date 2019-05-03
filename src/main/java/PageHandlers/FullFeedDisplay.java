e6b7375fba6681d9825deeee3ebbfa7be0353178
mandlerm <amaya.melissa@gmail.com>
2019-05-03 15:04:29 -0500

    code commenting and cleaning up in fullfeeddisplay.java

--- src/main/java/PageHandlers/FullFeedDisplay.java
+++ src/main/java/PageHandlers/FullFeedDisplay.java
@@ -7,25 +7,28 @@ import spark.Response;
 import spark.Route;
 
 /**
- * Handles the display of the full, unfiltered twitter feed
+ * Handles the display of the full, unfiltered twitter feed. Has a single backend end call and 
  * @author mamaya
  *
  */
 public class FullFeedDisplay implements Route{
 	private final String header = "<h1>Full Twitter Feed</h1>";
 	private final String homeButton = "<button type=\"button\" onclick=\"location.href='/'\">Home</button>";
-//	private final String seeHiddenFeedBtn = "<button type=\"button\" onclick=\"location.href='/hiddenFeed'\">All Clear. See What I Missed</button>";
 	private String tweets;
 	private final String refreshButton = "<form action=\"fullFeed\" method=\"get\"> "+
 			"<button type=\"submit\" value=\"feed_refresh\">Refresh my feed</button>" +
 			"</form></div></html>";
 	
-
+	/**
+	 * calls a method in the BackEndHandler class that retrieves a formatted string of unfiltered tweet objects
+	 */
 	public FullFeedDisplay() {
 		tweets = BackEndHandler.fullFeed();
 	}
-//	
-	
+
+	/**
+	 * Returns all the display objects for the browser
+	 */
 	public Object handle(Request request, Response response) throws Exception {
 		
 		return header + homeButton + refreshButton + tweets;

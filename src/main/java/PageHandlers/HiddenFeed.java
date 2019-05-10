001221a57a8c69e5dbc5cc76a6225faaee42e83e
mandlerm <amaya.melissa@gmail.com>
2019-05-09 20:22:54 -0500

    hidden feed class cleanup

--- src/main/java/PageHandlers/HiddenFeed.java
+++ src/main/java/PageHandlers/HiddenFeed.java
@@ -6,17 +6,12 @@ import spark.Route;
 
 public class HiddenFeed implements Route{
 	private String tweetInfo; 
-	
-	public HiddenFeed() {
-		
-	}
-	
+
 	//header for hidden page website 
 	private final String header = "<html><head><title>Spoiler Tweets</title><link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">" + 
-			"</head>" +
-			"<div text-align: center><h1>These are your spoiler tweets </h1></div>\n";
+			"</head>" + "<div text-align: center><h1>These are your spoiler tweets </h1></div>\n";
 	
-	//
+	
 	private final String bootstrapJS = "<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>\n" + 
 			"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>\n" + 
 			"<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>";

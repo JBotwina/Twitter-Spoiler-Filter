3e38be510e7845a4c6fb99c0e438a0b120650368
mandlerm <amaya.melissa@gmail.com>
2019-05-11 09:34:48 -0500

    commented hiddenfeed

--- src/main/java/PageHandlers/HiddenFeed.java
+++ src/main/java/PageHandlers/HiddenFeed.java
@@ -11,13 +11,15 @@ public class HiddenFeed implements Route{
 	private final String header = "<html><head><title>Spoiler Tweets</title><link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">" + 
 			"</head>" + "<div text-align: center><h1>These are your spoiler tweets </h1></div>\n";
 	
-	
+	// bootstrap import
 	private final String bootstrapJS = "<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>\n" + 
 			"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>\n" + 
 			"<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>";
-		 
+	
+	// home button
 	private final String homeButton = "<div><form action=\'\\' method =\"get\"><button type=\"submit\" class=\"btn btn-primary\">Home</button></form></div>";
 	
+	// retrieves needed tweets. interacts with backend handler
 	public String getTweetInfo() {
 		tweetInfo = BackEndHandler.tweetToString(BackEndHandler.getBadStatuses());
 		if (tweetInfo.length() < 136) {
@@ -30,6 +32,7 @@ public class HiddenFeed implements Route{
 		return tweetInfo;
 	}
 	
+	// returns display info
 	public Object handle(Request request, Response response) throws Exception {
 		return "<div class = \"container\">" + header + getKeyword() + homeButton + "</div><body>" + getTweetInfo() + bootstrapJS + "</body></html>";
 	}

5f0c3eaec324bef1df2123c5030789d529b298f3
mandlerm <amaya.melissa@gmail.com>
2019-05-12 21:17:11 -0500

    add @test to tests

--- src/test/java/PageHandlers/FilteredFeedTest.java
+++ src/test/java/PageHandlers/FilteredFeedTest.java
@@ -1,18 +1,23 @@
 package PageHandlers;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class FilteredFeedTest extends TestCase {
+	@Test
 	public void testGetHeader() {
 		FilteredFeed f = new FilteredFeed();
 		assertNotNull(f.getHeader()); 
 	}
 
+	@Test
 	public void testGetRefreshButton() {
 		FilteredFeed f = new FilteredFeed();
 		assertNotNull(f.getRefreshButton()); 
 	}
 
+	@Test
 	public void testGetHiddenFeedButton() {
 		FilteredFeed f = new FilteredFeed();
 		assertNotNull(f.getHiddenFeedButton()); 
--- src/test/java/PageHandlers/FullFeedDisplayTest.java
+++ src/test/java/PageHandlers/FullFeedDisplayTest.java
@@ -1,24 +1,30 @@
 package PageHandlers;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class FullFeedDisplayTest extends TestCase {
-
+	
+	@Test
 	public void testGetHeader() {
 		FullFeedDisplay f = new FullFeedDisplay();
 		assertNotNull(f.getHeader()); 
 	}
 
+	@Test
 	public void testGetHomeButton() {
 		FullFeedDisplay f = new FullFeedDisplay();
 		assertNotNull(f.getHomeButton()); 
 	}
 
+	@Test
 	public void testGetBootstrapJS() {
 		FullFeedDisplay f = new FullFeedDisplay();
 		assertNotNull(f.getBootstrapJS()); 
 	}
 	
+	@Test
 	public void testGetPullTweets() {
 		FullFeedDisplay f = new FullFeedDisplay();
 		assertNotNull(f.PullTweets()); 

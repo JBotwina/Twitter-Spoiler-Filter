c1909aaafd75022e318e61d6e3e77b253dff832f
mandlerm <amaya.melissa@gmail.com>
2019-05-12 22:10:43 -0500

    correct junit issue

--- src/test/java/backEnd/TwitterGeneratorTest2.java
+++ src/test/java/backEnd/TwitterGeneratorTest2.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-class TwitterGeneratorTest2 {
+public class TwitterGeneratorTest2 {
 	
 	TwitterGenerator tg = new TwitterGenerator();
 

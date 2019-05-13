8ee3a8cddbeb88c75df1c2784fa3443ec06195f2
mandlerm <amaya.melissa@gmail.com>
2019-05-12 21:45:56 -0500

    fixed backend tests

--- src/test/java/backEnd/TwitterGeneratorTest2.java
+++ src/test/java/backEnd/TwitterGeneratorTest2.java
@@ -1,8 +1,9 @@
 package backEnd;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class TwitterGeneratorTest2 {
 	
@@ -10,40 +11,40 @@ class TwitterGeneratorTest2 {
 
 	
 	@Test
-	void testGetUnfilteredStatuses() {
+	public void testGetUnfilteredStatuses() {
 		assertNotNull(tg.getUnfilteredStatuses());
 	}
 
 	@Test
-	void testGetSpoilerTweets() {
+	public void testGetSpoilerTweets() {
 		assertNotNull(tg.getSpoilerTweets());
 	}
 
 	@Test
-	void testGetGoodStatuses() {
+	public void testGetGoodStatuses() {
 		assertNotNull(tg.getGoodStatuses());
 	}
 
 	@Test
-	void testParseTweets() {
+	public void testParseTweets() {
 		tg.parseTweets("xxxxxxxxx");
 		assertNotNull(tg.getGoodStatuses());
 	}
 
 	@Test
-	void testCheckIfKeywordsMatchTweetedWord() {
+	public void testCheckIfKeywordsMatchTweetedWord() {
 		String[] names = {"james", "melissa", "jhon"};
 		assertTrue(tg.checkIfKeywordsMatchTweetedWord("james",names));
 	}
 	
 	@Test
-	void testCheckIfKeywordsMatchTweetedWord2() {
+	public void testCheckIfKeywordsMatchTweetedWord2() {
 		String[] names = {"james", "melissa", "jhon"};
 		assertTrue(!(tg.checkIfKeywordsMatchTweetedWord("steven",names)));
 	}
 	
 	@Test
-	void testCheckIfKeywordsMatchTweetedWord3() {
+	public void testCheckIfKeywordsMatchTweetedWord3() {
 		String[] name = {"Steven"};
 		assertTrue(tg.checkIfKeywordsMatchTweetedWord("steven",name));
 	}
